757618c39e93b25e4bebcebe7905ec66b2d83a96
IvanZou1 <ivanzou2@gmail.com>
2023-04-14 23:40:03 -0400

    add price and number formatters

--- client/src/helpers/formatter.js
+++ client/src/helpers/formatter.js
@@ -8,3 +8,17 @@ export function formatReleaseDate(date) {
   const dateObj = new Date(Date.parse(date));
   return dateObj.toLocaleString("en-US", { month: "long", day: "numeric", year: "numeric" });
 }
+
+export function formatUnitNumber(number) {
+  const formatter = new Intl.NumberFormat('en-US');
+  return formatter.format(number);
+}
+
+export function formatUnitPrice(price) {
+  const priceFormatter = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD', maximumFractionDigits: 0});
+  return priceFormatter.format(price);
+}
+
+export function formatPriceByThousand(price) {
+  return formatUnitPrice(price) + "k";
+}
