93b14713884c1035822d5380ace4ed09de6a6d8f
IvanZou1 <ivanzou2@gmail.com>
2023-04-15 11:33:30 -0400

    attempted to find counties and fill table, no work

--- client/src/pages/CountiesPage.js
+++ client/src/pages/CountiesPage.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 import { Button, Checkbox, Container, InputLabel, FormControl, FormControlLabel, Grid, MenuItem, Select, Slider, TextField, Typography } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
+
 import { formatUnitNumber, formatPriceByThousand} from '../helpers/formatter';
+const config = require('../config.json');
 
 export default function CountiesPage() {
     // Have a query find the earliest date and the highest date for bounds on the date (Hardcoded for now)
@@ -9,7 +11,7 @@ export default function CountiesPage() {
     const latest = 202302;
     // Array with all the years between earliest and latest
     const years = [];
-    for (let i = latest; i > earliest - 100; i -= 100) {
+    for (let i = latest; i >= earliest; i -= 100) {
         // Since of dates are of the form YYYYMM, we divide by 100 to get the year and increment by 100 to get the next year.
         years.push(Math.trunc(i / 100));
     }
@@ -20,7 +22,7 @@ export default function CountiesPage() {
     // State to keep track of input to search bar
     const [name, setName] = useState('');
     // States to keep track of the date of the data that will be used to query (initially 02/2023)
-    const [month, setMonth] = useState(latest % 100);
+    const [month, setMonth] = useState("" + latest % 100);
     const [year, setYear] = useState(years[0]);
     // State to keep track of bounds for average price when querying
     const [averagePrice, setAveragePrice] = useState([0, 10000000]);
@@ -34,6 +36,34 @@ export default function CountiesPage() {
     const [activeListingCount, setActiveListingCount] = useState([0, 24000]);
     // State to keep track of whether to sort by hotness score or not
     const [sortByHotness, setSortByHotness] = useState(false);
+
+    useEffect(() => {
+        fetch(`http://${config.server_host}:${config.server_port}/search_counties`)
+          .then(res => res.json())
+          .then(resJson => {
+            const countiesWithId = resJson.map((county) => ({ id: county.id, ...county }));
+            setData(countiesWithId);
+          });
+      }, []);
+
+      // filling the table does not currently work; do not know why
+      const search = () => {
+        fetch(`http://${config.server_host}:${config.server_port}/search_counties?name=${name}` +
+        `&average_price_low=${averagePrice[0]}&average_price_high=${averagePrice[1]}` +
+        `&supply_score_low=${supplyScore[0]}&supply_score_high=${supplyScore[1]}` +
+        `&demand_score_low=${demandScore[0]}&demand_score_high=${demandScore[1]}` +
+        `&median_square_feet_low=${medianSquareFeet[0]}&median_square_feet_high=${medianSquareFeet[1]}` +
+        `&active_listing_count_low=${activeListingCount[0]}&active_listing_count_high=${activeListingCount[1]}` +
+        `&sort_by_hotness=${sortByHotness}` +
+        `&year=${year}` +
+        `&month=${month}`
+        )
+        .then(res => res.json())
+        .then(resJson => {
+            const countiesWithId = resJson.map((county) => ({ id: county.id, ...county }));
+            setData(countiesWithId);
+        });
+      }
 
     // Columns for our result table
     const columns = [
@@ -73,18 +103,18 @@ export default function CountiesPage() {
                             onChange={(e) => setMonth(e.target.value)}
                             label="Month"   
                         >
-                            <MenuItem value={1}>Jan</MenuItem>
-                            <MenuItem value={2}>Feb</MenuItem>
-                            <MenuItem value={3}>Mar</MenuItem>
-                            <MenuItem value={4}>Apr</MenuItem>
-                            <MenuItem value={5}>May</MenuItem>
-                            <MenuItem value={6}>Jun</MenuItem>
-                            <MenuItem value={7}>Jul</MenuItem>
-                            <MenuItem value={8}>Aug</MenuItem>
-                            <MenuItem value={9}>Sep</MenuItem>
-                            <MenuItem value={10}>Oct</MenuItem>
-                            <MenuItem value={11}>Nov</MenuItem>
-                            <MenuItem value={12}>Dec</MenuItem>
+                            <MenuItem value={"01"}>Jan</MenuItem>
+                            <MenuItem value={"02"}>Feb</MenuItem>
+                            <MenuItem value={"03"}>Mar</MenuItem>
+                            <MenuItem value={"04"}>Apr</MenuItem>
+                            <MenuItem value={"05"}>May</MenuItem>
+                            <MenuItem value={"06"}>Jun</MenuItem>
+                            <MenuItem value={"07"}>Jul</MenuItem>
+                            <MenuItem value={"08"}>Aug</MenuItem>
+                            <MenuItem value={"09"}>Sep</MenuItem>
+                            <MenuItem value={"10"}>Oct</MenuItem>
+                            <MenuItem value={"11"}>Nov</MenuItem>
+                            <MenuItem value={"12"}>Dec</MenuItem>
                             
                         </Select>
                     </FormControl>
@@ -176,7 +206,7 @@ export default function CountiesPage() {
                 </Grid>
             </Grid>
             {/*Search Button*/}
-            <Button style={{ left: '50%', transform: 'translateX(-50%)', marginBottom: '30px' }}>
+            <Button onClick={() => search() } style={{ left: '50%', transform: 'translateX(-50%)', marginBottom: '30px' }}>
                 Search
             </Button>
             {/*Table with all the data*/}
--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ app.use(cors({
 // provide their handlers that we implemented in routes.js
 app.get('/author', routes.author);
 app.get('/county_listing_prices', routes.county_listing_prices);
+app.get('/search_counties', routes.search_counties);
 
 app.listen(config.server_port, () => {
   console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
