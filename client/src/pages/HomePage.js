8c57a2b984616235d6e41bc8e8d7cb4859557435
IvanZou1 <ivanzou2@gmail.com>
2023-04-28 00:10:04 -0400

    added remaining county info to the web page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Container, Divider, Link } from '@mui/material';
 import { NavLink } from 'react-router-dom';
-import { formatUnitPrice, formatCountyName } from '../helpers/formatter';
+import { formatUnitPrice, formatCountyName, formatUnitNumber, formatNull } from '../helpers/formatter';
 
 
 import LazyTable from '../components/LazyTable';
@@ -33,7 +33,7 @@ export default function HomePage() {
   }, []);
 
 
-  const countyColumns = [
+  const listingPrices = [
     {
       field: 'name',
       headerName: 'County',
@@ -51,11 +51,110 @@ export default function HomePage() {
     },
   ];
 
+  const squareFootage = [
+    {
+      field: 'name',
+      headerName: 'County',
+      renderCell: (row) => formatCountyName(row.name)
+    },
+    {
+      field: 'median_listing_price_per_square_foot',
+      headerName: 'Median Listing Price Per Square Foot',
+      renderCell: (row) => formatUnitNumber(row.median_listing_price_per_square_foot)
+    },
+    {
+      field: 'median_square_feet',
+      headerName: 'Median Square Feet',
+      renderCell: (row) => formatUnitNumber(row.median_square_feet)
+    },
+  ];
+
+  const listingCounts = [
+    {
+      field: 'name',
+      headerName: 'County',
+      renderCell: (row) => formatCountyName(row.name)
+    },
+    {
+      field: 'active',
+      headerName: 'Active Listings',
+      renderCell: (row) => formatUnitNumber(row.active)
+    },
+    {
+      field: 'new',
+      headerName: 'New Listings',
+      renderCell: (row) => formatUnitNumber(row.new)
+    },
+    {
+      field: 'total',
+      headerName: 'Total Listings',
+      renderCell: (row) => formatUnitNumber(row.total)
+    },
+  ];
+
+  const hotnessScores = [
+    {
+      field: 'name',
+      headerName: 'County',
+      renderCell: (row) => formatCountyName(row.name)
+    },
+    {
+      field: 'hotness',
+      headerName: 'Hotness Scores',
+      renderCell: (row) => formatNull(row.hotness)
+    },
+  ];
+
+  const supplyAndDemandScores = [
+    {
+      field: 'name',
+      headerName: 'County',
+      renderCell: (row) => formatCountyName(row.name)
+    },
+    {
+      field: 'demand',
+      headerName: 'Demand Scores',
+      renderCell: (row) => formatNull(row.demand)
+    },
+    {
+      field: 'supply',
+      headerName: 'Supply Scores',
+      renderCell: (row) => formatNull(row.supply)
+    },
+  ];
+
   return (
     <Container>
       <Divider />
       <h2>Latest Listing Prices</h2>
-      <LazyTable route={`http://${config.server_host}:${config.server_port}/latest_county_info`} columns={countyColumns} rowsPerPageOptions={[5, 10, 25]} />
+      <LazyTable 
+        route={`http://${config.server_host}:${config.server_port}/latest_county_info`} 
+        columns={listingPrices} 
+        rowsPerPageOptions={[5, 10, 25]} />
+      <Divider />
+      <h2>Latest Square Footage</h2>
+      <LazyTable 
+        route={`http://${config.server_host}:${config.server_port}/latest_county_info`} 
+        columns={squareFootage} 
+        rowsPerPageOptions={[5, 10, 25]} />
+      <Divider />
+      <h2>Latest Listing Count</h2>
+      <LazyTable 
+        route={`http://${config.server_host}:${config.server_port}/latest_county_info`} 
+        columns={listingCounts} 
+        rowsPerPageOptions={[5, 10, 25]} />
+      <Divider />
+      <h2>Latest Hotness Scores</h2>
+      <LazyTable 
+        route={`http://${config.server_host}:${config.server_port}/latest_county_info`} 
+        columns={hotnessScores} 
+        rowsPerPageOptions={[5, 10, 25]} />
+      <Divider />
+      <h2>Latest Supply and Demand Scores</h2>
+      <LazyTable 
+        route={`http://${config.server_host}:${config.server_port}/latest_county_info`} 
+        columns={supplyAndDemandScores} 
+        rowsPerPageOptions={[5, 10, 25]} />
       <Divider />
       <p>{appAuthor}</p>
     </Container>
