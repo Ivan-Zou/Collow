0de69b801cb18dc9ebbc41539299e1f3ad473762
IvanZou1 <ivanzou2@gmail.com>
2023-04-27 23:35:27 -0400

    make null values return "N/A"

--- client/src/helpers/formatter.js
+++ client/src/helpers/formatter.js
@@ -1,6 +1,6 @@
 export function formatUnitNumber(number) {
   if (number == null) {
-    return "";
+    return "N/A";
   }
   const formatter = new Intl.NumberFormat('en-US');
   return formatter.format(number);
@@ -8,7 +8,7 @@ export function formatUnitNumber(number) {
 
 export function formatUnitPrice(price) {
   if (price == null) {
-    return "";
+    return "N/A";
   }
   const priceFormatter = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD', maximumFractionDigits: 0});
   return priceFormatter.format(price);
@@ -16,17 +16,21 @@ export function formatUnitPrice(price) {
 
 export function formatPriceByThousand(price) {
   if (price == null) {
-    return "";
+    return "N/A";
   }
   return formatUnitPrice(price) + "k";
 }
 
 export function formatCountyName(name) {
   if (name == null) {
-    return "";
+    return "N/A";
   }
   const countyState = name.split(', ');
   const county = countyState[0];
   const state = countyState[1];
   return county[0].toUpperCase() + county.slice(1) + ", " + state.toUpperCase();
+}
+
+export function formatNull(value) {
+  return value == null ? "N/A" : value;
 }
