458d43c9bdf64d1e20910bc7a568d5fc26bd5443
IvanZou1 <ivanzou2@gmail.com>
2023-04-27 23:20:52 -0400

    made formatter handle null values

--- client/src/helpers/formatter.js
+++ client/src/helpers/formatter.js
@@ -1,18 +1,30 @@
 export function formatUnitNumber(number) {
+  if (number == null) {
+    return "";
+  }
   const formatter = new Intl.NumberFormat('en-US');
   return formatter.format(number);
 }
 
 export function formatUnitPrice(price) {
+  if (price == null) {
+    return "";
+  }
   const priceFormatter = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD', maximumFractionDigits: 0});
   return priceFormatter.format(price);
 }
 
 export function formatPriceByThousand(price) {
+  if (price == null) {
+    return "";
+  }
   return formatUnitPrice(price) + "k";
 }
 
 export function formatCountyName(name) {
+  if (name == null) {
+    return "";
+  }
   const countyState = name.split(', ');
   const county = countyState[0];
   const state = countyState[1];
