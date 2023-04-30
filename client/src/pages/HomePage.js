352d886fa820929c2cd46beb2d2968207f0d349b
IvanZou1 <ivanzou2@gmail.com>
2023-04-29 21:00:09 -0400

    delete temporary comments

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,35 +3,17 @@ import { Container, Divider, Link } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 import { formatUnitPrice, formatCountyName, formatUnitNumber, formatNull } from '../helpers/formatter';
 
-
 import LazyTable from '../components/LazyTable';
 const config = require('../config.json');
 
-
 export default function HomePage() {
-  // We use the setState hook to persist information across renders (such as the result of our API calls)
   const [appAuthor, setAppAuthor] = useState('');
-  const [selectedSongId, setSelectedSongId] = useState(null);
 
-
-  // States
-
-
-  // The useEffect hook by default runs the provided callback after every render
-  // The second (optional) argument, [], is the dependency array which signals
-  // to the hook to only run the provided callback if the value of the dependency array
-  // changes from the previous render. In this case, an empty array means the callback
-  // will only run on the very first render.
   useEffect(() => {
-    // Fetch request to get the song of the day. Fetch runs asynchronously.
-    // The .then() method is called when the fetch request is complete
-    // and proceeds to convert the result to a JSON which is finally placed in state.
-
     fetch(`http://${config.server_host}:${config.server_port}/author`)
       .then(res => res.text())
       .then(resText => setAppAuthor(resText));
   }, []);
-
 
   const listingPrices = [
     {
